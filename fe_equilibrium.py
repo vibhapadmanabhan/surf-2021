ca5142f4455a99a06875f4b4462a7f0e055c6d22
Vibha Padmanabhan <57306517+vibhapadmanabhan@users.noreply.github.com>
2021-07-07 16:24:29 -0700

    Update fe_equilibrium.py

--- fe_equilibrium.py
+++ fe_equilibrium.py
@@ -1,11 +1,12 @@
 import math
+from scipy.optimize import fsolve
 
-# constants 
-r_planet = 1000 * 10**3
-r_impactor = 100 * 10**3
+# constants
+r_planet = 1000 * 10E3
+r_impactor = 100 * 10E3
 rho_mantle = 3000 
 rho_core = 8000
-P_cmb = 136 * 0.7 # in GPa, assuming 70% of CMB pressure
+P_cmb = 0.7 * 25 # in GPa, assuming 70% of CMB pressure. CMB pressure depends on size of the planet.
 T_cmb = 4000
 
 # Mantle composition: chondritic, in wt%, ignoring trace elements.
@@ -15,8 +16,8 @@ si = 10.7
 fe = 85
 ni = 15
 
-planet_mantle_depth = 300 * 10**3  # given
-impactor_core_radius = 53.1329  * 10**3  # assuming core/mantle mass ratio to be the same as present day Earth's
+planet_mantle_depth = 300 * 10E3  # given
+impactor_core_radius = 53.1329  * 10E3  # assuming core/mantle mass ratio to be the same as present day Earth's
 impactor_mantle_depth = r_impactor - impactor_core_radius
 
 def calculate_shell_vol(mantle_depth, r_obj):
@@ -33,8 +34,8 @@ def convert_mass_to_moles(compound_mass, compound_molar_mass):
 
 impactor_mantle_mass = calculate_shell_vol(impactor_mantle_depth, r_impactor) * rho_mantle
 planet_mantle_mass = calculate_shell_vol(planet_mantle_depth, r_planet) * rho_mantle
-
 impactor_core_mass = calculate_vol(impactor_core_radius) * rho_core
+
 fe_mass = calculate_element_mass(impactor_core_mass, fe)
 ni_mass = calculate_element_mass(impactor_core_mass, ni)
 
@@ -51,33 +52,78 @@ def calculate_kd(metal, T, P, a, b, c):
         return 10**(a + b / T + c * P / T)
 
 # actual distribution constants from Rubie
-kd_si = calculate_kd("si", T_cmb, P_cmb, 2.98, -15934, None)
-kd_ni = calculate_kd("ni", T_cmb, P_cmb, 1.06, 1553, -98)
-print("Actual K_d for Ni-Fe: ", kd_ni)
-print("Actual K_d for Si-Fe: ", kd_si)
-
-
-# assume some initial values for mols of x in the metal phase (core)
-init_mol_fe_metal = 10**17
-init_mol_si_metal = 6 * 10**20
-
+actual_kd_si = calculate_kd("si", T_cmb, P_cmb, 2.98, -15934, None)
+actual_kd_ni = calculate_kd("ni", T_cmb, P_cmb, 1.06, 1553, -98)
+print("Actual k_d", actual_kd_ni, actual_kd_si)
 # mass balance
 def calculate_vals(mol_fe_metal, mol_si_metal, mol_si, mol_ni, mol_o, mol_fe):
+    """
+    Returns the number of moles of (FeO, Ni, NiO, SiO2) in a tuple given initial values for Fe and Si.
+    """
     mol_FeO = mol_fe - mol_fe_metal
     mol_SiO2 = mol_si - mol_si_metal
     mol_NiO = mol_o - 2 * mol_SiO2 - mol_FeO
     mol_ni_metal = mol_ni - mol_NiO
-    return ((mol_fe_metal, mol_FeO, mol_ni_metal, mol_NiO, mol_si_metal, mol_SiO2))
+    return ((mol_FeO, mol_ni_metal, mol_NiO, mol_SiO2))
 
-mol_fe_metal, mol_FeO, mol_ni_metal, mol_NiO, mol_si_metal, mol_SiO2 = calculate_vals(init_mol_fe_metal, init_mol_si_metal, mol_si, mol_ni, mol_o, mol_fe)
+def kd_ni(mol_fe_metal, mol_FeO, mol_ni_metal, mol_NiO, mol_si_metal, mol_SiO2):
+    """
+    Calculates K_d for the reaction NiO + Fe <--> FeO + Ni.
+    """
+    conc_fe_metal = mol_fe_metal / (mol_fe_metal + mol_si_metal + mol_ni_metal)
+    conc_FeO = mol_FeO / (mol_FeO + mol_NiO + mol_SiO2)
+    conc_ni_metal = mol_ni_metal / (mol_fe_metal + mol_si_metal + mol_ni_metal)
+    conc_NiO = mol_NiO / (mol_FeO + mol_NiO + mol_SiO2)
+    return conc_ni_metal * conc_FeO / conc_NiO / conc_fe_metal
 
-# print(mol_fe_metal, mol_FeO, mol_ni_metal, mol_NiO, mol_si_metal, mol_SiO2)
-conc_fe_metal = mol_fe_metal / (mol_fe_metal + mol_si_metal + mol_ni_metal)
-conc_FeO = mol_FeO / (mol_FeO + mol_NiO + mol_SiO2)
-conc_ni_metal = mol_ni_metal / (mol_fe_metal + mol_si_metal + mol_ni_metal)
-conc_NiO = mol_NiO / (mol_FeO + mol_NiO + mol_SiO2)
-conc_si_metal = mol_si_metal / (mol_fe_metal + mol_si_metal + mol_ni_metal)
-conc_SiO2 = mol_SiO2 / (mol_FeO + mol_NiO + mol_SiO2)
+def kd_si(mol_fe_metal, mol_FeO, mol_ni_metal, mol_NiO, mol_si_metal, mol_SiO2):
+    """
+    Calculates K_d for the reaction # SiO2 + 2Fe <--> 2FeO + Si.
+    """
+    conc_fe_metal = mol_fe_metal / (mol_fe_metal + mol_si_metal + mol_ni_metal)
+    conc_FeO = mol_FeO / (mol_FeO + mol_NiO + mol_SiO2)
+    conc_si_metal = mol_si_metal / (mol_fe_metal + mol_si_metal + mol_ni_metal)
+    conc_SiO2 = mol_SiO2 / (mol_FeO + mol_NiO + mol_SiO2)
+    return conc_si_metal * conc_FeO**2 / conc_SiO2 / conc_fe_metal**2
 
-print("Calculated value for Ni-Fe: ", conc_ni_metal * conc_FeO / conc_NiO / conc_fe_metal)
-print("Calculated value for Si-Fe: ", conc_si_metal * conc_FeO / conc_SiO2 / conc_fe_metal)
+# initial guesses for Fe and Si
+mol_fe_metal = mol_fe - 10E17
+mol_si_metal = 10E15
+
+# calculate other initial values
+mol_FeO, mol_ni_metal, mol_NiO, mol_SiO2 = calculate_vals(mol_fe, 0, mol_si, mol_ni, mol_o, mol_fe)
+
+# print initial values
+print("Initial values:")
+print(mol_fe_metal, mol_FeO, mol_ni_metal, mol_NiO, mol_si_metal, mol_SiO2)
+
+
+kd_ni_calc = kd_ni(mol_fe_metal, mol_FeO, mol_ni_metal, mol_NiO, mol_si_metal, mol_SiO2)
+
+# use Le Chatelier's principle to converge on correct values
+while kd_ni_calc < actual_kd_ni:
+    # print("loop")
+    mol_fe_metal += 10E17
+    mol_FeO, mol_ni_metal, mol_NiO, mol_SiO2 = calculate_vals(mol_fe_metal, mol_si_metal, mol_si, mol_ni, mol_o, mol_fe)
+    kd_si_calc = kd_si(mol_fe_metal, mol_FeO, mol_ni_metal, mol_NiO, mol_si_metal, mol_SiO2)
+    if kd_si_calc < actual_kd_si:
+        mol_si_metal += 10E17
+        mol_FeO, mol_ni_metal, mol_NiO, mol_SiO2 = calculate_vals(mol_fe_metal, mol_si_metal, mol_si, mol_ni, mol_o, mol_fe)
+    kd_ni_calc = kd_ni(mol_fe_metal, mol_FeO, mol_ni_metal, mol_NiO, mol_si_metal, mol_SiO2)
+
+
+print("final values: ", mol_fe_metal, mol_si_metal)
+print("calculated K_d: ", kd_ni(mol_fe_metal, mol_FeO, mol_ni_metal, mol_NiO, mol_si_metal, mol_SiO2), kd_si(mol_fe_metal, mol_FeO, mol_ni_metal, mol_NiO, mol_si_metal, mol_SiO2))
+# print("Calculated value for Ni-Fe: ", kd_ni(mol_fe_metal, mol_FeO, mol_ni_metal, mol_NiO, mol_si_metal, mol_SiO2))
+
+# print("Calculated value for Si-Fe: ", kd_si(mol_fe_metal, mol_FeO, mol_ni_metal, mol_NiO, mol_si_metal, mol_SiO2))
+
+
+# x[0] is Fe_m, x[1] is FeO, x[2] is Si_m, x[3] is SiO, x[4] is Ni_m, x[5] is NiO
+# def func(x, mol_fe, mol_ni, mol_si, mol_o, kd_ni, kd_si):
+#    return [x[0] + x[1] - mol_fe, x[2] + x[3] - mol_si, x[4] + x[5] - mol_ni, x[1] + x[3] + x[5] - mol_o, 
+#    x[4] / (x[4] + x[0] + x[2]) * (x[1] / (x[1] + x[3] + x[5])) / (x[5] / (x[1] + x[2] + x[3])) / (x[0] / (x[2] + x[4] + x[0])) - kd_ni, 
+#    x[2] / (x[4] + x[0] + x[2]) * (x[1] / (x[1] + x[3] + x[5]))**2 / (x[3] / (x[1] + x[2] + x[3])) / (x[0] / (x[2] + x[4] + x[0]))**2 -  #kd_si]
+
+#root = fsolve(func, [10E19, 10E18, 10E17, 10E21, 10E20, 10E10], (mol_fe, mol_ni, mol_si, mol_o, kd_ni, kd_si))
+#print(root)
