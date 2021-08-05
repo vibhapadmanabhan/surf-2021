d11638e4ae1948934dd5037a12c7409b402e352e
Vibha Padmanabhan <57306517+vibhapadmanabhan@users.noreply.github.com>
2021-08-05 10:38:23 -0700

    renamed

--- rapid_solidification.py
+++ rapid_solidification.py
@@ -1,4 +1,4 @@
-from fe_equilibrium import *
+from equilibrium import *
 from growth import *
 
 planet_core_radius = core_radius(r_planet)
@@ -18,7 +18,7 @@ mols_fe_c = 1000 * fe * 0.01 * calculate_vol(planet_core_radius) * rho_core / mo
 
 v_metal = 0
 i = 0
-while r_planet <= 1000e3:
+for i in range(100):
     print(i)
     planet_size.append(r_planet)
     r_impactor = r_planet / 10
@@ -31,15 +31,15 @@ while r_planet <= 1000e3:
     delivered_v = 1000 * v_s * 0.01 * ocean_mass(r_impactor, r_impactor - c_impactor) / (molar_mass_v)
     delivered_o = 1000 * o_s * 0.01 * ocean_mass(r_impactor, r_impactor - c_impactor) / (molar_mass_o)
     h = calculate_h(melt_factor * calculate_vol(r_impactor), r_planet)
-    print(h)
-    mantle_depth.append(h)
+    # print(h)
+    # mantle_depth.append(h)
     # h_frac is the fraction of the mantle that is molten (magma ocean)
     h_frac = calculate_shell_vol(h, r_planet) / calculate_shell_vol(r_planet - planet_core_radius, r_planet)
     planet_mass = convert_moles_to_mass(mol_fe, molar_mass_fe) + convert_moles_to_mass(mol_ni, molar_mass_ni) + convert_moles_to_mass(mol_si, molar_mass_si) + convert_moles_to_mass(mol_v, molar_mass_v) + convert_moles_to_mass(mol_o, molar_mass_o) + convert_moles_to_mass(mol_mg, molar_mass_mg) + convert_moles_to_mass(mols_fe_c, molar_mass_fe) + convert_moles_to_mass(mols_ni_c, molar_mass_ni) + convert_moles_to_mass(mols_si_c, molar_mass_si) + convert_moles_to_mass(mols_ni_c, molar_mass_ni) + convert_moles_to_mass(mols_v_c, molar_mass_v)
     g_acc = calculate_g(planet_mass)
     gravity.append(g_acc)
     
-    print("gravity", g_acc)
+    # print("gravity", g_acc)
     
     # calculate compounds already present in mantle up till melted depth assuming homogeneous mantle
     mol_si_MO = h_frac * mol_si
@@ -66,10 +66,10 @@ while r_planet <= 1000e3:
     # equilibrium and mass balance
     P_eq = Peq(g_acc, h)
     pressure.append(P_eq)
-    print("pressure", P_eq)
+    # print("pressure", P_eq)
     T_eq = Teq(P_eq * 1e9)
     temperature.append(T_eq)
-    print("temperature", T_eq)
+    # print("temperature", T_eq)
     # small error due to exclusion of delta h from volume of impactor mantle (does not change results)
     fe_metal = bisection_search(f, root_bracket(f, mol_fe_MO, mol_ni_MO, mol_si_MO, mol_o_MO, mol_v_MO, mol_mg_MO, P_eq, T_eq, v_metal), mol_fe_MO, 10e-7, mol_fe_MO, mol_ni_MO, mol_si_MO, mol_o_MO, mol_v_MO, mol_mg_MO, P_eq, T_eq, v_metal)
     fe_sil = mol_fe_MO - fe_metal
@@ -107,7 +107,7 @@ while r_planet <= 1000e3:
     r_planet += added_core_depth
     added_mantle_depth = added_depth(r_planet, added_mantle_mass, rho_mantle)
     r_planet += added_mantle_depth
-    print(r_planet)
+    # print(r_planet)
 
     # update total moles in mantle of elements involved in equilibrium based on number of moles left in the silicate phase
     mol_fe += fe_sil - mol_fe * h_frac
@@ -118,7 +118,7 @@ while r_planet <= 1000e3:
     # calculate fugacity
     X_FeO.append(mol_fe / (mol_fe + mol_ni + mol_si + mol_v / 2 + mol_mg))
     X_Fe.append(mols_fe_c / (mols_fe_c + mols_si_c + mols_ni_c + mols_v_c))
-    print("molar conc of Fe in core", X_Fe)
+    # print("molar conc of Fe in core", X_Fe)
     fO2.append(calculate_ln_o_iw_fugacity(X_FeO[-1], X_Fe[-1]))
 
     # track other values
@@ -132,8 +132,24 @@ while r_planet <= 1000e3:
 
 save_data(X_FeO, X_Fe, X_SiO2, X_Si, X_Va, X_VO, X_Ni, X_NiO, pressure, temperature, gravity, planet_size, fO2, mantle_depth)
 
+# plot fugacity
 plt.plot([r / 1e3 for r in planet_size], fO2)
 plt.xlabel("Planet radius (km)")
 plt.ylabel("ln(fO2) - IW")
 plt.title("Fugacity vs. planet radius")
 plt.show()
+
+# plot Si molar concentration in core
+plt.plot([r / 1e3 for r in planet_size], X_Si)
+plt.xlabel("Planet radius (km)")
+plt.ylabel("[Si]_m")
+plt.title("Molar concentration of Si in core vs. planet radius")
+plt.show()
+
+# plot FeO molar concentration in mantle
+plt.plot([r / 1e3 for r in planet_size], X_FeO)
+plt.xlabel("Planet radius (km)")
+plt.ylabel("[FeO]_sil")
+plt.title("Molar concentration of FeO in mantle vs. planet radius")
+plt.show()
+
